3340cffd07f6a29f9340ce41e581f681a50cc108
myk1118 <jaimekim1118@gmail.com>
2019-12-13 02:22:59 +0000

    Setup client and created images endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 const apiRouter = require("./api");
+const imagesRouter = require("./images");
 
 // Routes for /
+
 // ALL METHODS USE: /api
 router.use("/api", apiRouter);
+
+// ALL METHODS /images
+router.use("/images", imagesRouter);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
-const db = require("./db");
-const {
-    buildUrl
-} = require("./helpers");
+// const db = require("./db");
+// const {
+//     buildUrl
+// } = require("./helpers");
 const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, "client", "dist")));
+
 app.use(routes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
 
 // sample product id: 6145a47a-13cb-11ea-8574-0242ac120002
 

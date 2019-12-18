9a2c727227531109488630d3a859710740af28c3
myk1118 <jaimekim1118@gmail.com>
2019-12-18 07:06:24 +0000

    Quick fix

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "client", "dist")));
 app.use(express.urlencoded({
     extended: false
 }));
+app.use(express.static(path.resolve(__dirname, "client", "dist")));
 
 app.use(routes);
 
